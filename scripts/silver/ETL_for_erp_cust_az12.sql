526c9bd014023f1e56b56a43e0143e81a0038729
Vishwanath Reddy <114723190+Vishwanath-reddy-01@users.noreply.github.com>
2025-12-21 18:11:06 +0530

    ETL_pipeline_for_erp_cust_az12

---
+++ scripts/silver/ETL_for_erp_cust_az12.sql
@@ -0,0 +1,58 @@
+/*
+===============================================================================
+Sql script: Load Data to erp_cust_az12 (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This sql script performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema table from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver table.
+		- Inserts transformed and cleansed data from Bronze into Silver table.
+		
+===============================================================================
+*/
+
+DECLARE @start_time DATETIME, @end_time DATETIME; 
+  BEGIN TRY
+  
+	PRINT '------------------------------------------------';
+	PRINT 'Loading erp_cust_az12 Table';
+	PRINT '------------------------------------------------';
+
+        -- Loading erp_cust_az12
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_cust_az12';
+		TRUNCATE TABLE silver.erp_cust_az12;
+		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
+		INSERT INTO silver.erp_cust_az12 (
+			cid,
+			bdate,
+			gen
+		)
+		SELECT
+			CASE
+				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
+				ELSE cid
+			END AS cid, 
+			CASE
+				WHEN bdate > GETDATE() THEN NULL
+				ELSE bdate
+			END AS bdate, -- Set future birthdates to NULL
+			CASE
+				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+				ELSE 'n/a'
+			END AS gen -- Normalize gender values and handle unknown cases
+		FROM bronze.erp_cust_az12;
+	    SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';	
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
