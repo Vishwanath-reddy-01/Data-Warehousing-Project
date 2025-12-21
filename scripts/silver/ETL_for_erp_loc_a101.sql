02bece30e49675144a81e2770f17a91bbeeb8494
Vishwanath Reddy <114723190+Vishwanath-reddy-01@users.noreply.github.com>
2025-12-21 18:16:16 +0530

    ETL_pipeline_for_erp_loc_a101

---
+++ scripts/silver/ETL_for_erp_loc_a101.sql
@@ -0,0 +1,52 @@
+/*
+===============================================================================
+Sql script: Load Data to erp_loc_a101 (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This sql script performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema table from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver table.
+		- Inserts transformed and cleansed data from Bronze into Silver table.
+		
+===============================================================================
+*/
+
+DECLARE @start_time DATETIME, @end_time DATETIME; 
+  BEGIN TRY
+  
+	PRINT '------------------------------------------------';
+		PRINT 'Loading erp_loc_a101 Table';
+		PRINT '------------------------------------------------';
+
+        -- Loading erp_loc_a101
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.erp_loc_a101';
+		TRUNCATE TABLE silver.erp_loc_a101;
+		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
+		INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
+	    SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+			
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
