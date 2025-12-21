4ba284de1a8c8f7330b9f442f5f58d46df905ebd
Vishwanath Reddy <114723190+Vishwanath-reddy-01@users.noreply.github.com>
2025-12-21 18:05:44 +0530

    ETL_pipeline_for_crm_sales_details.sql

---
+++ scripts/silver/ETL_for_crm_sales_details.sql
@@ -0,0 +1,87 @@
+/*
+===============================================================================
+Sql script: Load Data to crm_sales_details (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This sql script performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema table from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver table.
+		- Inserts transformed and cleansed data from Bronze into Silver table.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+===============================================================================
+*/
+
+DECLARE @start_time DATETIME, @end_time DATETIME; 
+  BEGIN TRY
+  
+	PRINT '------------------------------------------------';
+	PRINT 'Loading crm_sales_details Table';
+	PRINT '------------------------------------------------';
+	
+	 -- Loading crm_sales_details
+    SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+		PRINT '>> Inserting Data Into: silver.crm_sales_details';
+		INSERT INTO silver.crm_sales_details (
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT 
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+      
+			CASE 
+				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END AS sls_order_dt,
+      
+			CASE 
+				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END AS sls_ship_dt,
+      
+			CASE 
+				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END AS sls_due_dt,
+      
+			CASE 
+				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+					THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+			
+      sls_quantity,
+			
+      CASE 
+				WHEN sls_price IS NULL OR sls_price <= 0 
+					THEN sls_sales / NULLIF(sls_quantity, 0)
+				ELSE sls_price  -- Derive price if original value is invalid
+			END AS sls_price
+      
+		FROM bronze.crm_sales_details;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
