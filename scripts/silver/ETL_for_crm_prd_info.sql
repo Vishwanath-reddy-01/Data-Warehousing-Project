4fe28d55fb45c723882adbcea1173c02b93727dc
Vishwanath Reddy <114723190+Vishwanath-reddy-01@users.noreply.github.com>
2025-12-21 17:59:21 +0530

    ETL_pipeline_for_crm_prd_info.sql

---
+++ scripts/silver/ETL_for_crm_prd_info.sql
@@ -0,0 +1,69 @@
+/*
+===============================================================================
+Sql script: Load Data to crm_prd_info (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This sql script performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema table from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver table.
+		- Inserts transformed and cleansed data from Bronze into Silver table.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+===============================================================================
+*/
+DECLARE @start_time DATETIME, @end_time DATETIME; 
+  BEGIN TRY
+  
+		PRINT '------------------------------------------------';
+		PRINT 'Loading crm_prd_info Table';
+		PRINT '------------------------------------------------';
+
+-- Loading silver.crm_prd_info
+    SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_prd_info';
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>> Inserting Data Into: silver.crm_prd_info';
+		INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
+				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+END TRY
+BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+END CATCH
